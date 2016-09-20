840d108ac87736a226190b1ee142766e1564a28b
Devayan Mandal <devayanmandal@gmail.com>
2016-09-20 14:22:04 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -189,7 +189,7 @@
 	/*----------------------------------------------------*/
 	/* Final Countdown Settings
 	------------------------------------------------------ */
-	var finalDate = '2016/09/21';
+	var finalDate = '2016/09/22';
 
 	$('div#counter').countdown(finalDate)
    	.on('update.countdown', function(event) {
